96eb3ba97a91af7f153427516be5861e6a3214fe
Dorisa Shehi <doris.shehi2@gmail.com>
2024-06-07 19:59:51 +0200

    Fixed problem with section

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // Import our custom CSS
 import './styles/main.scss'
 import './assets/images/pngfood2.png'
+import {eggLoader} from './assets/images/egg.gif';
 
 import getHome from './Components/Home/index';
 
